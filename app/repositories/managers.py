095c553d4b67376cf0b89cf3e3953a7535738239
MichaelAndrik <michaelandrik10@gmail.com>
2023-01-23 17:59:36 -0500

    chore: fix lint errors

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -63,8 +63,15 @@ class OrderManager(BaseManager):
         cls.session.add(new_order)
         cls.session.flush()
         cls.session.refresh(new_order)
-        cls.session.add_all((OrderDetail(order_id=new_order._id, ingredient_id=ingredient._id, ingredient_price=ingredient.price)
-                             for ingredient in ingredients))
+        cls.session.add_all(
+            (
+                OrderDetail(
+                    order_id=new_order._id,
+                    ingredient_id=ingredient._id,
+                    ingredient_price=ingredient.price
+                ) for ingredient in ingredients
+            )
+        )
         cls.session.commit()
         return cls.serializer().dump(new_order)
 
